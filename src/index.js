1bfcdf352fc49de488c05606f7749aa63a6b352c
nizamuddin3080 <mdnizamuddin3080@gmail.com>
2020-10-28 01:28:48 -0700

    React JS Mini-Project Creating Simple Greeting Website

--- src/index.js
+++ src/index.js
@@ -2,25 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css"
 
-const name = "Nizam Uddin";
-const img1 = "https://picsum.photos/200/300";
-const img2 = "https://picsum.photos/200/300";
-const img3 = "https://picsum.photos/200/300";
-const img4 = "https://picsum.photos/200/300";
-const link = "https://google.com";
+
+let currDate = new Date(20, 10,28, 15);
+currDate = currDate.getHours();
+let greeting = " ";
+const cssStyle = {  }
+
+if (currDate >=1 && currDate < 12) {
+  greeting = "Good Morning"
+  cssStyle.color = 'green';
+  
+} else if(currDate >= 12 && currDate < 19) {
+  greeting = "Good Afternoon";
+  cssStyle.color = 'yellow';
+
+} else {
+  greeting = "Good Night";
+  cssStyle.color = 'black';
+}
+
 
 ReactDOM.render(
   <>
+  <div className="div">
+    <h1>Hello Sir, <span style= {cssStyle}>{greeting}</span>    </h1>
+  </div>
 
-    <h1 className="heading">My name is {name}</h1>
-    <div className="div_image">
-      <a href={link} target=" " >
-        <img src={img1} alt="Random Image" />
-        <img src={img2} alt="Random Image" />
-        <img src={img3} alt="Random Image" />
-        <img src={img4} alt="Random Image" />
-      </a>
-    </div>
 
 
 
