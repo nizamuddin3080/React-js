2b28a1844042946ff86659dc7a6609494a244f19
nizamuddin3080 <mdnizamuddin3080@gmail.com>
2020-10-28 04:54:02 -0700

    React JS Mini-Project Creating Simple Greeting Website update (Functional Component)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import Heading from './Heading';
-import Para from './Para';
-import Order from './Order';
 
 function App() {
+  let currDate = new Date(20, 10,28, 15);
+currDate = currDate.getHours();
+let greeting = " ";
+const cssStyle = {  }
+
+if (currDate >=1 && currDate < 12) {
+  greeting = "Good Morning"
+  cssStyle.color = 'green';
+  
+} else if(currDate >= 12 && currDate < 19) {
+  greeting = "Good Afternoon";
+  cssStyle.color = 'yellow';
+
+} else {
+  greeting = "Good Night";
+  cssStyle.color = 'black';
+}
   return (
-    <>
-      <Heading />
-      <Para />
-      <Order />
-      <Heading />
-    </>
+    <div className="div">
+    <h1>Hello Sir, <span style= {cssStyle}>{greeting}</span></h1>
+    </div>
   );
 }
 
